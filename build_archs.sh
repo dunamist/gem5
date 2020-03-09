b7588927a13e185d22df7c9dc9e7de1c1cf1570f
DUNAMIST <dunamist@126.com>
2020-03-09 14:58:45 +0800

    update
    Change-Id: I06845e55dbbb6cdafef86d79d64be9e4c9438571

--- build_archs.sh
+++ build_archs.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
 scons build/ALPHA/gem5.opt -j5
-scons build/ALPHA_MESI_Two_Level/gem5.opt -j5
-scons build/ALPHA_MOESI_CMP_directory/gem5.opt -j5
 
 scons build/ARM/gem5.opt -j5
 
 scons build/X86/gem5.opt -j5
-scons build/X86_MESI_Two_Level/gem5.opt -j5
-scons build/X86_MOESI_AMD_Base/gem5.opt -j5
--- test_archs.sh
+++ test_archs.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
 build/ALPHA/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/alpha/linux/hello
-build/ALPHA_MESI_Two_Level/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/alpha/linux/hello
-build/ALPHA_MOESI_CMP_directory/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/alpha/linux/hello
 
 build/ARM/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/arm/linux/hello
 
 build/X86/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/x86/linux/hello
-build/X86_MESI_Two_Level/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/x86/linux/hello
-build/X86_MOESI_AMD_Base/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/x86/linux/hello
