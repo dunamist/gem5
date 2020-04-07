3f9d4a19c503393aab61ab27d24d924e81559759
DUNAMIST <dunamist@126.com>
2020-04-07 11:11:03 +0800

    update
    Change-Id: I2ab9210dc6eabb480ed5d4f5772411fe457fc249

--- 6_cpus/a8.sh
+++ 6_cpus/a8.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-~/gem5/build/ARM/gem5.opt ~/gem5/configs/example/se.py -c ~/gem5/tests/test-progs/hello/bin/arm/linux/hello --cpu-type=MinorCPU --cpu-clock=800MHz --caches --cacheline_size=32 --l1d_size=32kB --l1d_assoc=4 --l1i_size=32kB --l1i_assoc=4 --l2cache --l2_size=256kB --l2_assoc=8 --cacheline_size=32 --bp-type=TournamentBP
+~/gem5/build/ARM/gem5.opt ~/gem5/configs/example/se.py -c ~/gem5/tests/test-progs/hello/bin/arm/linux/hello --cpu-type=MinorCPU --cpu-clock=800MHz --caches --cacheline_size=32 --l1d_size=32kB --l1d_assoc=4 --l1i_size=32kB --l1i_assoc=4 --l2cache --l2_size=256kB --l2_assoc=8 --bp-type=TournamentBP
--- 6_cpus/alpha21164.sh
+++ 6_cpus/alpha21164.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-~/gem5/build/ALPHA/gem5.opt ~/gem5/configs/example/se.py -c ~/gem5/tests/test-progs/hello/bin/alpha/linux/hello --cpu-type=MinorCPU --cpu-clock=500MHz --caches --cacheline_size=32 --l1d_size=8kB --l1d_assoc=1 --l1i_size=8kB --l1i_assoc=1 --l2cache --l2_size=96kB --l2_assoc=3 --l3_size=4MB --l3_assoc=1 --cacheline_size=32 --bp-type=BiModeBP
+~/gem5/build/ALPHA/gem5.opt ~/gem5/configs/example/se.py -c ~/gem5/tests/test-progs/hello/bin/alpha/linux/hello --cpu-type=MinorCPU --cpu-clock=500MHz --caches --cacheline_size=32 --l1d_size=8kB --l1d_assoc=1 --l1i_size=8kB --l1i_assoc=1 --l2cache --l2_size=96kB --l2_assoc=3 --l3_size=4MB --l3_assoc=1 --bp-type=BiModeBP
